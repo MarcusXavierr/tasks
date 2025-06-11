e999e6579c75f3f0c8019445602d885fe877700f
Marcus Xavier <marcusxavierr123@gmail.com>
2025-06-11 17:42:49 -0300

    feat(TaskController): add task listing feature in index method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * Display a listing of tasks
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('created_at', 'desc')->get();
+        
+        return Inertia::render('Dashboard', [
+            'tasks' => $tasks
+        ]);
+    }
 }
