1232c0956f76a4ef2bbc5c05f830616cf87900d8
Marcus Xavier <marcusxavierr123@gmail.com>
2025-06-12 10:25:52 -0300

    feat(tasks): add task creation modal component and logic

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -37,12 +37,6 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Dashboard
                                 </NavLink>
-                                <NavLink
-                                    :href="route('tasks.create')"
-                                    :active="route().current('tasks.create')"
-                                >
-                                    Create Task
-                                </NavLink>
                             </div>
                         </div>
 
@@ -149,12 +143,6 @@ const showingNavigationDropdown = ref(false);
                             :active="route().current('dashboard')"
                         >
                             Dashboard
-                        </ResponsiveNavLink>
-                        <ResponsiveNavLink
-                            :href="route('tasks.create')"
-                            :active="route().current('tasks.create')"
-                        >
-                            Create Task
                         </ResponsiveNavLink>
                     </div>
 
